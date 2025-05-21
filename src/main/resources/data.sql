003dc83eff4c14aac70c5e8967fe5656d7b6b054
dikshahegde <hegdediksha24@gmail.com>
2025-05-21 14:11:14 +0530

    Updation of postgre

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,15 @@
 INSERT INTO doctors (name, spec, fees, hosp, city, exp)
-VALUES ('Dr Mohan Patil', 'cardiologist', 500, 'Patil Hospital', 'dandeli', '25 years');
+VALUES 
+('Dr. Nagendra Dande', 'Pulmonology', 500, 'ESI Hospital', 'Dandeli', '5 years'),
+('Dr. Roonali Bhatkal', 'Ayurveda', 700, 'ESI Hospital', 'Dandeli','6 years'),
+('Dr. Sadanand Karki', 'General Surgery', 400, 'SADANAND KARKI CLINIC', 'Dandeli', '8 years'),
+('Dr. Rekha Hegde', 'Ayurveda', 1000, 'Auyurvedic Hospital', 'Dandeli','6 years'),
+('Dr. Anil Naik', 'General Practice', 800, 'ESI Hospital', 'Dandeli', '8 years'),
+('Dr. Nagveni Gabi', 'Gynecology', 600, 'Gabi Nursing Home', 'Dandeli', '6 years'),
+('Dr. ABC', 'Occupational Medicine', 500, 'WCPM Hosp', 'Dandeli', '8 years'),
+('Dr. Rajesh Prasad', 'General Practice', 900, 'Hosp', 'Dandeli', '25 years'),
+('Dr. Ratnakar Kurdekar', 'General Practice', 300, 'Kulkarni Hospital', 'Dandeli', '8 years'),
+('Dr. R. K. Kulkarni', 'General Practice', 800, 'Patil Hospital', 'Dandeli', '25 years'),
+('Dr. Mohan', 'General Surgery', 600, 'Hosp', 'Dandeli', '25 years'),
+('Dr. Rahul Khubchandani', 'General Surgery', 300, 'Hosp', 'Dandeli', '8 years'),
+('Dr. Anshuman Kaushal', 'General Surgery', 800, 'Hosp', 'Dandeli', '25 years');
